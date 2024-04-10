1443b3a925ff217f7842a9de32ef3b315fe7f856
kashyrskyy <andriy.kashyrskyy@minerva.kgi.edu>
2024-04-10 14:48:35 -0400

    Trigger re-deployment to fix environment issue.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Providing a default value if VITE_DEPLOY_ENV is not set
-const deployEnv = import.meta.env.VITE_DEPLOY_ENV || 'default';
-console.log('Deploy Environment:', deployEnv);
-
-const base = deployEnv === 'github' ? '/ate-micr/' : '/';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base,
+  base: '/',
   plugins: [react()],
 })
