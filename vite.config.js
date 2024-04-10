09db7bc7081dc03ba6f795729688ac46227d5fbd
kashyrskyy <andriy.kashyrskyy@minerva.kgi.edu>
2024-04-10 14:17:28 -0400

    Trigger re-deployment with console error logging.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // Providing a default value if VITE_DEPLOY_ENV is not set
 const deployEnv = import.meta.env.VITE_DEPLOY_ENV || 'default';
+console.log('Deploy Environment:', deployEnv);
+
 const base = deployEnv === 'github' ? '/ate-micr/' : '/';
 
 // https://vitejs.dev/config/
