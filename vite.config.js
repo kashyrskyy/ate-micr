f0e33219183d1744f9a69c8c64aa3aa2b7e668c9
kashyrskyy <andriy.kashyrskyy@minerva.kgi.edu>
2024-04-10 15:18:44 -0400

    Changing vite.config.js for develop branch.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = import.meta.env.VITE_DEPLOY_ENV === 'github' ? '/ate-micr/' : '/';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base,
+  base: '/',
   plugins: [react()],
 })
