fbdf0f8adbea8e0a10294e83e1d6a4ef3c175ef3
kashyrskyy <andriy.kashyrskyy@minerva.kgi.edu>
2024-10-04 13:40:29 -0400

    Added initialization of Google Analytics for the project.

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -9,6 +9,7 @@ interface ImportMetaEnv {
     readonly VITE_MESSAGING_SENDER_ID: string;
     readonly VITE_APP_ID: string;
     readonly VITE_DEPLOY_ENV: string;
+    readonly VITE_GOOGLE_ANALYTICS_ID: string;
 }
 
 interface ImportMeta {
