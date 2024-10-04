299ad98ea44bac993e83244a3484ad7825a7880e
kashyrskyy <andriy.kashyrskyy@minerva.kgi.edu>
2024-10-04 14:16:02 -0400

    Trigger re-deployment.

--- src/components/Dashboard/ReportBugButton.tsx
+++ src/components/Dashboard/ReportBugButton.tsx
@@ -15,7 +15,7 @@ const ReportBugButton: React.FC = () => {
             className="report-bug-button"
             startIcon={<BugReportIcon />}
         >
-            Leave Feedback
+            Provide Feedback
         </Button>
     );
 };
